d3db5d0bf3676edef1540304e48cdf480ed50a4a
Omarlee18 <omar.ouattara.via@gmail.com>
2022-03-02 13:55:49 +0100

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -2,4 +2,5 @@ facebook on a besoin de vous pour votre générosité
 
 
             <p>pour suivre le contenue sur les news, trouver nous sur <a href="facebook">Facebook</a> et aussi <a href="intagram">Intagram</a> </p>
-   
+<img src="https://cdn.pixabay.com/photo/2021/06/25/12/26/social-media-6363633_960_720.jpg">
+<img src="https://cdn.pixabay.com/photo/2017/12/02/14/38/contact-us-2993000__340.jpg">
